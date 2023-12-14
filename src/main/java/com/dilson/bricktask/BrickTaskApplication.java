c31847054d09e7d2192aeb0bac2fcbefa1f5eda1
dilsoncampelo10 <dilsono316@gmail.com>
2023-12-14 13:19:04 -0300

    :tada: init project

---
+++ src/main/java/com/dilson/bricktask/BrickTaskApplication.java
@@ -0,0 +1,13 @@
+package com.dilson.bricktask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrickTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrickTaskApplication.class, args);
+	}
+
+}
