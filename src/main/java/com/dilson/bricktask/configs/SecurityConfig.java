c0efd3b94769c3827e4b9bfa902e9ebd47f88ef9
dilsoncampelo10 <dilsono316@gmail.com>
2023-12-17 13:25:29 -0300

    security configuration

---
+++ src/main/java/com/dilson/bricktask/configs/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.dilson.bricktask.configs;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfig {
+    private static final String[] PUBLIC_MATCHERS = {
+            "/"
+    };
+
+    private static final String[] PUBLIC_MATCHERS_POST = {
+            "/login",
+            "/user"
+    };
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable());
+        http.cors(cors -> cors.disable());
+        http.authorizeHttpRequests(
+                httpAuth -> httpAuth.requestMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+                        .requestMatchers(PUBLIC_MATCHERS).permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedMethods(Arrays.asList("POST, GET", "DELETE", "PUT"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
